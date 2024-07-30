d6ea0ae5d54d824c04fdc2f287b476ee040b6819
ingalls <nick@ingalls.ca>
2024-07-30 15:41:54 -0600

    Add Builder

---
+++ task.ts
@@ -0,0 +1,78 @@
+import { FeatureCollection } from 'geojson';
+import { Type, TSchema } from '@sinclair/typebox';
+import ETL, { TaskLayer, Event, SchemaType, handler as internal, local, env } from '@tak-ps/etl';
+
+const Environment = Type.Object({
+    URL: Type.String(),
+    QueryParams: Type.Array(Type.Object({
+        key: Type.String(),
+        value: Type.String()
+    })),
+    Headers: Type.Array(Type.Object({
+        key: Type.String(),
+        value: Type.String()
+    }))
+});
+
+export default class Task extends ETL {
+    async schema(type: SchemaType = SchemaType.Input): Promise<TSchema> {
+        if (type === SchemaType.Input) {
+            return Environment
+        } else {
+            return Type.Object({})
+        }
+    }
+
+    async control(): Promise<void> {
+        const env = await this.env(Environment);
+
+        const url = new URL(env.URL);
+
+        for (const param of env.QueryParams) {
+            url.searchParams.append(param.key, param.value);
+        }
+
+        const headers: Record<string, string> = {};
+        for (const header of env.Headers) {
+            headers[header.key] = header.value;
+        }
+
+        const res = await fetch(url, {
+            method: 'GET',
+            headers
+        });
+
+        // TODO: Type the response
+        const body: any = await res.json();
+
+        if (body.type !== 'FeatureCollection') {
+            throw new Error('Only FeatureCollection is supported');
+        }
+
+        const fc: FeatureCollection = {
+            type: 'FeatureCollection',
+            features: []
+        };
+
+        for (const feat of body.features) {
+            fc.features.push({
+                type: 'Feature',
+                properties: {
+                    metadata: feat.properties
+                },
+                geometry: feat.geometry
+            });
+        }
+
+        console.log(`ok - obtained ${fc.features.length} features`);
+
+        await this.submit(fc);
+    }
+}
+
+env(import.meta.url)
+await local(new Task(), import.meta.url);
+export async function handler(event: Event = {}) {
+    return await internal(new Task(), event);
+}
+
